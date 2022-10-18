641ad6d302475f703d4874cd644b096928787bbd
peng.ji <peng.ji@smartx.com>
2022-10-18 19:24:37 +0800

    add mod svt_process and hostname

---
+++ apis/src/svt_process.rs
@@ -0,0 +1,13 @@
+use sysinfo::{ProcessExt, System, SystemExt};
+
+#[cfg(any(target_os = "linux", target_os = "windows"))]
+#[no_mangle]
+pub extern "C" fn check_svt_process() -> i32 {
+    let s = System::new_all();
+    let mut check_res = 1;
+    for process in s.processes_by_name("qemu-ga") {
+        println!("{} {}", process.pid(), process.name());
+        check_res = 0;
+    }
+    check_res
+}
